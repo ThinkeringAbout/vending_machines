33ce82e3db6bff3327ee7f9214e53c391506beee
ThinkeringAbout <104573860+ThinkeringAbout@users.noreply.github.com>
2022-11-20 16:59:52 +0300

    project initial

---
+++ client/src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="input_container">
+    <h1>Список</h1>
+    <input class="search_input" type="text">
+  </div>
+  <div class="list">
+    <div class="wrapper custom_scroll">
+      <div class="list__item"></div>
+      <div class="list__item"></div>
+      <div class="list__item"></div>
+      <div class="list__item"></div>
+      <div class="list__item"></div>
+      <div class="list__item"></div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+:root {
+  --green-color: #76c893;
+  --blue-color: rgb(90, 90, 255);
+  --bg-color: #76c893;
+}
+
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Inter';
+}
+
+body {
+  width: 100%;
+  min-height: 100vh;
+  background: var(--bg-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  color: white;
+  text-shadow: 3px 2px 1px black;
+}
+
+.input_container {
+  width: 800px;
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 10px;
+}
+
+.search_input {
+  padding: 10px;
+  font-family: 'Inter';
+  font-weight: bold;
+  outline: none;
+  border: 3px solid black;
+  width: 250px;
+  border-radius: 15px;
+  box-shadow: 3px 3px 1px black;
+  transition: border .2s ease-in-out;
+}
+
+.input_container input:focus {
+  border: 3px solid rgb(90, 90, 255);
+}
+
+.list {
+  width: 800px;
+  margin: 0 auto;
+  border: 2px solid black;
+  background-color: var(--blue-color);
+  height: 90vh;
+  border-radius: 20px;
+  box-shadow: 6px 3px 3px black;
+  padding: 5px;
+}
+
+.wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  padding: 20px 0;
+  overflow-y: auto;
+}
+
+.list__item {
+  width: 80%;
+  min-height: 180px;
+  border: 3px solid black;
+  border-radius: 15px;
+  background: #fff;
+  box-shadow: 4px 2px 3px black;
+}
+
+.custom_scroll {
+  scroll-behavior: smooth;
+}
+
+.custom_scroll::-webkit-scrollbar {
+  width: 14px;
+}
+
+.custom_scroll::-webkit-scrollbar-track {
+  border-radius: 20px;
+  background-color: rgb(185, 183, 183);
+}
+
+.custom_scroll::-webkit-scrollbar-thumb {
+  background-color: black;
+  border-radius: 20px;
+}
+
+.custom_scroll::-webkit-scrollbar-thumb:hover {
+  border: 1px solid #fff;
+}
+</style>
